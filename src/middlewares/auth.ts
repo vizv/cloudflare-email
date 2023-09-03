5f6dedf47c9bb5ef86975c2e8fe9dfd07a57c551
dimaguy <dima@linkpuff.me>
2023-09-04 00:07:08 +0100

    env is not a thing sir

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,13 +7,13 @@ const AuthMiddleware = (request: Request, env: Env) => {
 	const token = request.headers.get('Authorization');
 
 	// Strict check for token existence
-	if (!env.TOKEN || env.TOKEN.length === 0) {
+	if (!TOKEN || TOKEN.length === 0) {
 		return new Response('You must set the TOKEN environment variable.', {
 			status: 401,
 		});
 	}
 
-	if (token !== env.TOKEN) {
+	if (token !== TOKEN) {
 		return new Response('Unauthorized', { status: 401 });
 	}
 };
